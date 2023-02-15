f94027797b8740f23a27d78c0132487db3206b50
Angelika Jackowska <jackowska.angelika1904@gmail.com>
2023-02-15 11:17:28 +0100

    zadanie4

--- zadanie4/readFile.js
+++ zadanie4/readFile.js
@@ -1,5 +1,55 @@
-const fs = require('fs')
+const fs = require('fs').promises
 const process = require('process')
+const axios = require('axios')
 
-const data = fs.readFileSync('file.js', 'utf-8')
-console.log(data)
+async function getJsonData(file) {
+	let data
+	try {
+		data = await fs.readFile(file)
+	} catch (err) {
+		throw new Error(`Cannot read file ${file}`)
+	}
+
+	try {
+		return JSON.parse(data)
+	} catch (err) {
+		throw new Error(`Cannot parse file content`)
+	}
+}
+
+async function getNumberInfo(number) {
+	if (isNaN(number)) {
+		throw new Error('Not a number')
+	}
+
+	try {
+		const response = await axios.get(`http://numbersapi.com/${number}`)
+		return response.data
+	} catch (err) {
+		throw new Error(`API Request unsuccessful`)
+	}
+}
+
+async function writeToFile(path, data) {
+	if (path === '') {
+		throw new Error(`File path empty`)
+	}
+
+	try {
+		await fs.writeFile(path, data)
+	} catch (err) {
+		throw new Error(`Cannot write to file ${path}`)
+	}
+}
+
+;(async () => {
+	try {
+		const sourceFile = process.argv[3] ?? 'data.json'
+		const { number, filename } = await getJsonData(sourceFile)
+		const data = await getNumberInfo(number)
+		await writeToFile(filename, data)
+		console.log(`Data for ${number} saved in ${filename}`)
+	} catch (error) {
+		console.log('Failed with:', error)
+	}
+})()
